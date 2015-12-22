e0130b4511341c8f290dec0763fb5e476e6f66ad
Pchiu <phillipychiu@gmail.com>
2015-12-21 18:30:18 -0800

    Changed ship and projectile movement
    Modified ship and projectile movement to move directly instead of using
    the physics engine.

--- Assets/Scripts/GuidedProjectile.cs
+++ Assets/Scripts/GuidedProjectile.cs
@@ -5,12 +5,11 @@ public class GuidedProjectile : Projectile {
 
 	public Ship target;
 	public float rotationSpeed;
-	public float accelerationAngleThreshold;
 
 	// Use this for initialization
 	void Start () {
 		Destroy (transform.gameObject, maxDuration);
-		rigidbody2D.AddForce(new Vector2(transform.up.x * launchVelocity, transform.up.y * launchVelocity));
+		//GetComponent<Rigidbody2D>().AddForce(new Vector2(transform.up.x * launchVelocity, transform.up.y * launchVelocity));
 	}
 	
 	// Update is called once per frame
@@ -25,12 +24,7 @@ public class GuidedProjectile : Projectile {
 			Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, target.transform.position - transform.position);
 			transform.rotation = Quaternion.RotateTowards (transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
-			if (Mathf.Abs (targetRotation.eulerAngles.z - transform.rotation.eulerAngles.z) < accelerationAngleThreshold)
-			{
-				rigidbody2D.AddForce(new Vector2(transform.up.x * acceleration, transform.up.y * acceleration));
-			}
-			Debug.Log ("Target " + targetRotation.eulerAngles);
-			Debug.Log ("Local " + transform.rotation.eulerAngles);
 		}
+        transform.position += new Vector3(transform.up.x * acceleration * Time.deltaTime, transform.up.y * acceleration * Time.deltaTime, 0);
 	}
 }
